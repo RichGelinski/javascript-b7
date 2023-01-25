5b2701c6c18c3f4761570442d41aabef7f063159
Felipe Gonçalves <114199894+felipgs@users.noreply.github.com>
2023-01-25 05:46:42 -0300

    modulo-8 aulas 011 a 013 concluidas

---
+++ modulo-8/aula012/script.js
@@ -0,0 +1,11 @@
+// document.querySelector('.texto').offsetHeight: mostra o tamanho que o elemento tem de altura (incluindo padding e margin e border)
+
+// document.querySelector('.texto').offsetwidth: mostra o tamanho que o elemento tem de largura (inclui padding, margin e barra de rolagem)
+
+//document.querySelector('.texto').clientHeight: mostra o tamanho que o elemento tem de altura (inclui apenas o conteúdo puro e padding)
+
+//document.querySelector('.texto').clientWidth: mostra o tamanho que o elemento tem de largura (inclui apenas padding)
+
+//document.querySelector('.texto').scrollHeight: mostra o tamando real da altura do elemento  (inclui todo o espaço disponível e barra de rolagem)
+
+//document.querySelector('.texto').scrollwidth: mostra o tamanho real da largura do elemento (não inclui barra de rolagem)
---
+++ modulo-8/aula013/script.js
@@ -0,0 +1,24 @@
+function subirTela() {
+    window.scrollTo({ //objeto criado para suavizar a rolagem do scroll 
+        top: 0,
+        behavior: "smooth",
+    })
+}
+
+document.addEventListener('scroll', rolar) //aciona evento quando rolar o scroll
+function rolar () {
+    let btn = document.getElementById('botao')
+    let posicao = window.scrollY
+    if (posicao >= 40) {
+        btn.style.display = 'block'
+    }   else {
+        btn.style.display = 'none'
+    }
+}
+
+
+/*
+window.scrollY ou window.scrollX : pega posição do scroll da página
+
+.scrollTop ou .scrollLeft : pega posição do scroll de um elemento
+*/
